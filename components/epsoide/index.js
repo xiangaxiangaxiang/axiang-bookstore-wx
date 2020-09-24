47a13702d04dd105c0d69ed8d70de8f2c0ede837
xiangaxiangaxiang <872632899@qq.com>
2020-09-24 22:00:23 +0800

    episode

---
+++ components/epsoide/index.js
@@ -0,0 +1,44 @@
+// components/epsoide/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    index: {
+      type: Number,
+      observer(newVal, oldVal, changePath) {
+        let val = newVal.toString().padStart(2, '0')
+        this.setData({
+          _index: val
+        })
+      }
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    months: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+    year: 1,
+    month: '七月',
+    _index: ''
+  },
+
+  attached() {
+    const date = new Date()
+    const year = date.getFullYear()
+    const month = date.getMonth()
+    this.setData({
+      year: year,
+      month: this.data.months[month]
+    })
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
