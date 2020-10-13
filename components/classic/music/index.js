1f3c8fd85d15dbf97e9e72dff97ec4e881d28f85
xiangaxiangaxiang <872632899@qq.com>
2020-10-13 22:48:15 +0800

    音乐页面

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,11 +1,17 @@
 // components/classic/music/index.js
 import { classicBeh } from '../classic-beh.js'
+
+const mMgr = wx.getBackgroundAudioManager()
+
+
 Component({
   behaviors: [classicBeh],
   /**
    * 组件的属性列表
    */
   properties: {
+    src: String,
+    title: String
   },
 
   /**
@@ -17,10 +23,63 @@ Component({
     playSrc: 'images/player@play.png',
   },
 
+  attached(event) {
+    // 跳转页面 当前 切换
+    this._recoverStatus()
+    this._monitorSwitch()
+  },
+
+  detached (event) {
+    // mMgr.stop()
+  },
+
   /**
    * 组件的方法列表
    */
   methods: {
+    onPlay (event) {
+      // 图片要切换
+      if (!this.data.playing) {
+        this.setData({
+          playing: true
+        })
+        mMgr.src = this.properties.src
+        mMgr.title = this.properties.title
+      } else {
+        this.setData({
+          playing: false
+        })
+        mMgr.pause()
+      }
+    },
 
+    _recoverStatus() {
+      if (mMgr.paused) {
+        this.setData({
+          playing: false
+        })
+        return
+      }
+      if (mMgr.src == this.properties.src) {
+        this.setData({
+          playing: true
+        })
+      }
+    },
+
+    _monitorSwitch () {
+      mMgr.onPlay(() => {
+        this._recoverStatus()
+      })
+      mMgr.onPause(() => {
+        this._recoverStatus()
+      })
+      mMgr.onStop(() => {
+        this._recoverStatus()
+      })
+      mMgr.onEnded(() => {
+        this._recoverStatus()
+      })
+    }
   }
 })
